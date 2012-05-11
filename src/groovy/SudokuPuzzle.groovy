825e7c90396dba0395992022fa3e178166670393
David James <david@dcgroovy.org>
2012-05-11 16:24:01 -0400

    Tests to ensure valid puzzle input

--- src/groovy/SudokuPuzzle.groovy
+++ src/groovy/SudokuPuzzle.groovy
@@ -7,6 +7,30 @@ class SudokuPuzzle {
   }
 
   void setTableData(Integer[][] values) {
+    ensureValidInput(values)
     tableData = values
+  }
+
+  private void ensureValidInput(Integer[][] values) {
+    ensureRowSize(values)
+    ensureValidColumn(values)
+  }
+
+  private void ensureRowSize(Integer[][] values) {
+    if (values.length != 9)
+      throw new IllegalArgumentException()
+  }
+
+  private void ensureValidColumn(Integer[][] data) {
+    for (row in data) {
+      if (row.length != 9)
+        throw new IllegalArgumentException()
+      if (!row.every {col -> isValidValue(col)})
+        throw new IllegalArgumentException()
+    }
+  }
+
+  private boolean isValidValue(Integer cell) {
+    return cell == null || (1..9).contains(cell)
   }
 }
--- test/groovy/SudokuTests.groovy
+++ test/groovy/SudokuTests.groovy
@@ -4,11 +4,43 @@ class SudokuTests extends Specification {
 
   SudokuPuzzle sudoku = new SudokuPuzzle()
 
-  def "a sudoku puzzle with no values should not be declared solved"() {
+  def "an empty sudoku puzzle should not be declared solved"() {
     when:
     sudoku.setTableData(new Integer[9][9])
 
     then:
     !sudoku.isSolved()
+  }
+
+  def "should throw exception if table data is not a 9x9 array"() {
+    when:
+    sudoku.setTableData(new Integer[rows][cols])
+
+    then:
+    thrown(IllegalArgumentException)
+
+    where:
+    rows | cols
+    0    | 0
+    8    | 9
+    9    | 10
+  }
+
+  def "should throw exception when a cell value is not in the range 1..9 or null"() {
+    given:
+    def data = new Integer[9][9]
+    data[row][col] = value
+
+    when:
+    sudoku.setTableData(data)
+
+    then:
+    thrown(IllegalArgumentException)
+
+    where:
+    row | col | value
+    0   | 0   | 0
+    8   | 8   | 10
+    1   | 8   | -1
   }
 }
